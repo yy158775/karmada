7c74ba03e525366294a7a30cfd6ed4206ba66844
yangyong <1584616775@qq.com>
2022-08-23 14:48:07 +0800

    test-8-e2e-only

--- hack/util.sh
+++ hack/util.sh
@@ -390,6 +390,12 @@ function util::kubectl_with_retry() {
     for i in {1..10}; do
         kubectl "$@"
         ret=$?
+        
+        echo "ulimit:"
+        ulimit -n
+        echo "all fd:"
+        cat /proc/sys/fs/file-nr
+
         if [[ ${ret} -ne 0 ]]; then
             echo "kubectl $@ failed, retrying(${i} times)"
             sleep 1
